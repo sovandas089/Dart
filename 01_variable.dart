16324fdca38042ccd8285898a5e657ada2eed93f
sovandas089 <sovandas089@gmail.com>
2025-01-24 10:58:03 +0530

    variables Commit

---
+++ 01_variable.dart
@@ -0,0 +1,12 @@
+void main() {
+  details();
+}
+
+void details() {
+  String name = "Sovan";
+  int age = 23;
+  bool isStudent = true;
+  List<String> favLanguages = ["Flutter & Dart", "Java", "JavaScript", "C"];
+  print(
+      "The details of $name is \n age: $age \n $name is Student: $isStudent \n $name 's Favourite Programming Languages are : $favLanguages \n");
+}

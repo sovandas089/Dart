b25df9aa0b9cb8bbdb48bbd0b2379ab34a36d6f2
sovandas089 <sovandas089@gmail.com>
2025-01-24 11:26:23 +0530

    variable

--- 01_variable.dart
+++ 01_variable.dart
@@ -6,7 +6,15 @@ void details() {
   String name = "Sovan";
   int age = 23;
   bool isStudent = true;
-  List<String> favLanguages = ["Flutter & Dart", "Java", "JavaScript", "C"];
+  // In case of Dynamic List we can pass both Strings and Integers
+  //in String list we can only pass the Strings
+  List<dynamic> favLanguages = [
+    "Flutter & Dart",
+    "Java",
+    "JavaScript",
+    "C",
+    44
+  ];
   print(
       "The details of $name is \n age: $age \n $name is Student: $isStudent \n $name 's Favourite Programming Languages are : $favLanguages \n");
 }
